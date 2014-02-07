a0f21b3b621f4c6da909c996605aa5ea2f48ca67
Przybylski 중광 <r351574nc3@gmail.com>
2014-02-07 04:42:30 -0700

    Initializing database

--- src/main/java/trnapp/bookstore/BookOrderForm.java
+++
@@ -1,9 +0,0 @@
-package trnapp.bookstore;
-
-import org.kuali.rice.krad.web.form.TransactionalDocumentFormBase;
-
-public class BookOrderForm extends TransactionalDocumentFormBase {
-
-	private static final long serialVersionUID = 1005929406016713579L;
-
-}
